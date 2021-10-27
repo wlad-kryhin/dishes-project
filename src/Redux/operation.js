5bf2384388da1e23b1e65d1786d518bc29b34116
Wlad <wladkrychin@gmail.com>
2021-10-27 10:55:04 +0200

    Update operation.js

--- src/Redux/operation.js
+++ src/Redux/operation.js
@@ -1,8 +1,8 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
 export const fetchDishes = createAsyncThunk("fetchDishes", async (values) => {
   try {
     const { data } = await axios.post(
